d7eaaf26ebe754074ab373db32d6e4a860472023
alyconr <alyconr@hotmail.com>
2023-07-03 20:39:12 -0500

    :wrench: Add babel and update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,11 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
         test: /\.css$/i,
         use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
