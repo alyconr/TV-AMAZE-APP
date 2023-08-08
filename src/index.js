99ea8307a5e400578da3cc0c25981f2b2a1158d8
alyconr <alyconr@hotmail.com>
2023-08-08 09:15:01 -0500

    :zap: Improve performance

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-import { getShows } from "./modules/getShows.js";
-import { renderShows } from "./modules/renderShows.js";
+import { renderShows } from './modules/renderShows.js';
+import './assets/images/heartnobg.png';
+import './assets/images/heartbg.png';
+import './assets/images/icons8-user-default-64.png';
+import './assets/images/movie.png';
+import './assets/images/cancelmodal.svg';
+import { showModals } from './modules/renderModals.js';
+import { renderActionMovies } from './modules/renderAction.js';
+import { searchMovies } from './modules/searchMovies.js';
 
-getShows();
 renderShows();
-
-
+showModals();
+renderActionMovies();
+searchMovies();
