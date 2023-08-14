3467ed01706d1f045dee0be43539646d3c808a2a
alyconr <alyconr@hotmail.com>
2023-08-14 15:42:29 -0500

    :globe_with_meridians: Add Express server to handle routes

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Serve static assets from the 'dist' directory
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Route all requests to the main HTML file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
